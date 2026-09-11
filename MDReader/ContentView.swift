bf650839f63499abf1980a5f987a629a4ce7def7
Zac <850047874@qq.com>
2026-09-11 13:35:51 +0800

    Initial Commit

---
+++ MDReader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MDReader
+//
+//  Created by 郎震 on 2026/9/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MDReader/MDReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MDReaderApp.swift
+//  MDReader
+//
+//  Created by 郎震 on 2026/9/11.
+//
+
+import SwiftUI
+
+@main
+struct MDReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
